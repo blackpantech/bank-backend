9facffbc2ca0d210cca7898f8a7a4ccdf24ac8d4
blackpantech <pantech@blackpantech.com>
2025-07-12 13:59:19 +0200

    explicit mapping

--- src/main/java/com/blackpantech/bank/infra/jpa/transaction/TransactionEntityMapper.java
+++ src/main/java/com/blackpantech/bank/infra/jpa/transaction/TransactionEntityMapper.java
@@ -2,12 +2,14 @@ package com.blackpantech.bank.infra.jpa.transaction;
 
 import com.blackpantech.bank.domain.transaction.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TransactionEntityMapper {
 
+    @Mapping(source = "account.id", target = "accountId")
     Transaction TransactionEntityToTransaction(final TransactionEntity transactionEntity);
 
     List<Transaction> TransactionEntitiesToTransactions(final List<TransactionEntity> transactionEntities);
